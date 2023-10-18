24aaa20a38bbd2de87754b7ca02fe75af56b411c
emsi06001 <emsi06001@E-PW0234N3>
2023-10-18 12:47:02 +0200

    Add project files.

---
+++ inluppg4.4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace inluppg4_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Välj ett av följande alternativ:\n1. Subtrahera två tal\n2. Dividera ett tal med ett annat\n3. Avsluta programmet");
+                int menuSelection = int.Parse(Console.ReadLine());
+
+                if (menuSelection == 1)
+                {
+                    Console.WriteLine("Skriv första talet");
+                    float num1 = float.Parse(Console.ReadLine());
+                    Console.WriteLine("Skriv första talet");
+                    float num2 = float.Parse(Console.ReadLine());
+                    Console.WriteLine("Svaret blir: " + (num1 - num2) + "\n\n\n\n");
+                }
+                else if (menuSelection == 2)
+                { 
+                    Console.WriteLine("Skriv första talet");
+                    float num1 = float.Parse(Console.ReadLine());
+                    Console.WriteLine("Skriv första talet");
+                    float num2 = float.Parse(Console.ReadLine());
+                    Console.WriteLine("Svaret blir: " + (num1 / num2) + "\n\n\n\n");
+                }
+                else if ( menuSelection == 3)
+                {
+                    return;
+                }
+            }
+        }
+    }
+}
